8cc0f4254c52f9ca9bab59a19287178d0bc75408
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-12-07 22:37:58 +0000

    Removed more redundancies

--- src/main.h
+++
@@ -1,8 +0,0 @@
-﻿// OpenKrauzeEngine.h : Include file for standard system include files,
-// or project specific include files.
-
-#pragma once
-
-#include <iostream>
-
-// TODO: Reference additional headers your program requires here.
