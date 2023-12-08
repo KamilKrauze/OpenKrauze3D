23b3b9687e546fb934479dbe78023337c69eac98
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-12-08 18:46:09 +0000

    Add OMP thread count

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,14 @@
 #pragma comment (lib, "opengl32.lib")
 
+#include <thread>
+
 #include "App.h"
+#include <omp.h>
 
 int main() {
+	const unsigned int threads = std::thread::hardware_concurrency();
+	omp_set_num_threads(threads);
+
 	App* app = new App();
 	return app->run();
 }
