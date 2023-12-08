d3248f192918df1ed20c84764e1eac4121b5d9f4
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-12-08 15:48:04 +0000

    Changed minor OpenGL version

--- src/App.cpp
+++ src/App.cpp
@@ -62,9 +62,9 @@ void App::display()
 
 int App::run()
 {
-	//glfwWindowHint(GLFW_SAMPLES, 8);
+	glfwWindowHint(GLFW_SAMPLES, 8);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 	glfwWindowHint(GLFW_DOUBLEBUFFER, GLFW_TRUE);
 
