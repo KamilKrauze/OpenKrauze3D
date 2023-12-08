ea108596869e61f868dd9f0cbf30be51b779fb18
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-12-08 14:23:36 +0000

    Minimal include header
    Makes it easier by not having to redefine basic header includes

---
+++ src/Core/minimal.h
@@ -0,0 +1,15 @@
+#ifndef MINIMAL_H
+#define MINIMAL_H
+
+// Standard libraries
+#include <iostream>
+#include <exception>
+
+// Thirdparty libraries
+#include <glad/glad.h>
+
+// Owned libraries
+#include "Sanitize.h"
+
+#endif // !MINIMAL_H
+
