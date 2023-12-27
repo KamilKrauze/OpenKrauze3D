01c417532ab72a4a9ad45845c62042c1c153a1c6
Kamil Krauze <kamila.p.krauze@gmail.com>
2023-12-27 18:53:30 +0000

    Added globals for camera controls

---
+++ src/Public/Globals/Controls.h
@@ -0,0 +1,21 @@
+#ifndef CONTROLS_H
+#define CONTROLS_H
+
+namespace OKE3D {
+	namespace Globals {
+
+		namespace Controls {
+			
+			namespace Camera {
+				inline float speed = 2.5f;
+
+				inline float general_sensitivity = 0.05f;
+				inline float horizontal_sensitivity = 0.05f;
+				inline float vertical_sensitivity = 0.05f;
+			}
+		}
+
+	};
+};
+
+#endif // !CONTROLS_H
